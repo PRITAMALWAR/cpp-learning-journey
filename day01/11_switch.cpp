adcd1b80d3ff5672169d5c7739715b4e667320e9
PRITAMVERMA <pritamkumar08102004@gmail.com>
2026-08-24 18:00:31 +0530

    switch statement

---
+++ day01/11_switch.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+int main()
+{
+    int day;
+
+    std::cout << "Enter day number (1-7): ";
+    std::cin >> day;
+
+    switch (day)
+    {
+        case 1:
+            std::cout << "Monday";
+            break;
+
+        case 2:
+            std::cout << "Tuesday";
+            break;
+
+        case 3:
+            std::cout << "Wednesday";
+            break;
+
+        case 4:
+            std::cout << "Thursday";
+            break;
+
+        case 5:
+            std::cout << "Friday";
+            break;
+
+        case 6:
+            std::cout << "Saturday";
+            break;
+
+        case 7:
+            std::cout << "Sunday";
+            break;
+
+        default:
+            std::cout << "Invalid day";
+    }
+
+    return 0;
+}
