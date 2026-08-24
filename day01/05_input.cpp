425e7845e70a3315fafd2889378bae6e198cdc65
PRITAMVERMA <pritamkumar08102004@gmail.com>
2026-08-24 11:32:03 +0530

    day 01 learn user input

---
+++ day01/05_input.cpp
@@ -0,0 +1,46 @@
+// #include <iostream>
+
+// int main()
+// {
+//     int age;
+
+//     std::cout << "Enter your age: ";
+//     std::cin >> age;
+
+//     std::cout << "Your age is: " << age;
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <iostream>
+
+int main()
+{
+    int a;
+    int b;
+
+    std::cout << "Enter two numbers: ";
+    std::cin >> a >> b;
+
+    std::cout << "A = " << a << std::endl;
+    std::cout << "B = " << b << std::endl;
+
+    return 0;
+}
+
+
+
+
