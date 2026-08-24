8d97ce3982d431aa9673eb8f907478bc049f633b
PRITAMVERMA <pritamkumar08102004@gmail.com>
2026-08-24 14:25:44 +0530

    day 01 learn arithmetic operators

---
+++ day01/06_operators.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+int main()
+{
+    int a = 20;
+    int b = 6;
+
+    std::cout << "Addition: " << a + b << std::endl;
+    std::cout << "Subtraction: " << a - b << std::endl;
+    std::cout << "Multiplication: " << a * b << std::endl;
+    std::cout << "Division: " << a / b << std::endl;
+    std::cout << "Remainder: " << a % b << std::endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include <iostream>
+
+// int main()
+// {
+//     int a;
+//     int b;
+
+//     std::cout << "Enter first number: ";
+//     std::cin >> a;
+
+//     std::cout << "Enter second number: ";
+//     std::cin >> b;
+
+//     std::cout << "Addition: " << a + b << std::endl;
+//     std::cout << "Subtraction: " << a - b << std::endl;
+//     std::cout << "Multiplication: " << a * b << std::endl;
+//     std::cout << "Division: " << a / b << std::endl;
+//     std::cout << "Remainder: " << a % b << std::endl;
+
+//     return 0;
+// }
