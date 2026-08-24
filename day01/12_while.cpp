55244787ecc7ab41a946ad55c4e9381150281475
PRITAMVERMA <pritamkumar08102004@gmail.com>
2026-08-24 18:04:32 +0530

    learn while loop

---
+++ day01/12_while.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+int main()
+{
+    int i = 1;
+
+    while (i <= 5)
+    {
+        std::cout << i << std::endl;
+
+        i++;
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include <iostream>
+
+// int main()
+// {
+//     int n;
+
+//     std::cout << "Enter a number: ";
+//     std::cin >> n;
+
+//     int i = 1;
+
+//     while (i <= n)
+//     {
+//         std::cout << i << std::endl;
+
+//         i++;
+//     }
+
+//     return 0;
+// }
