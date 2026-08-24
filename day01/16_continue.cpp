f80c761bcd7df7d9553352f8dcf84bdd3dec4342
PRITAMVERMA <pritamkumar08102004@gmail.com>
2026-08-24 18:35:45 +0530

    learn continue statement

---
+++ day01/16_continue.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        if (i == 3)
+        {
+            continue;
+        }
+
+        std::cout << i << std::endl;
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include <iostream>
+
+// int main()
+// {
+//     for (int i = 1; i <= 10; i++)
+//     {
+//         if (i % 2 == 0)
+//         {
+//             continue;
+//         }
+
+//         std::cout << i << std::endl;
+//     }
+
+//     return 0;
+// }
