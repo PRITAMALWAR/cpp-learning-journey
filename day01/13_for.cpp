555d987901316a83a1c98d09953ea72cef74633e
PRITAMVERMA <pritamkumar08102004@gmail.com>
2026-08-24 18:10:30 +0530

    learn for loop

---
+++ day01/13_for.cpp
@@ -0,0 +1,33 @@
+// #include <iostream>
+
+// int main()
+// {
+//     for (int i = 1; i <= 5; i++)
+//     {
+//         std::cout << i << std::endl;
+//     }
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+// Count 1 to 10
+
+
+#include <iostream>
+
+int main()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        std::cout << i << std::endl;
+    }
+
+    return 0;
+}
