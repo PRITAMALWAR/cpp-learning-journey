ede34d79177912916c176fb41b51ba07d930c0b4
PRITAMVERMA <pritamkumar08102004@gmail.com>
2026-08-24 10:14:43 +0530

    day 01 add cout and endl

---
+++ day01/02_print.cpp
@@ -0,0 +1,26 @@
+// #include <iostream>
+
+// int main()
+// {
+//     std::cout << "My name is Rahul";
+//     std::cout << "I am doing MCA";
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "My name is Rahul" << std::endl;
+    std::cout << "I am doing MCA" << std::endl;
+
+    return 0;
+}
