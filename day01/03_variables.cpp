8b5c75f134599a55835688b104fea6cff191266f
PRITAMVERMA <pritamkumar08102004@gmail.com>
2026-08-24 10:18:29 +0530

    day 01 learn variables

---
+++ day01/03_variables.cpp
@@ -0,0 +1,59 @@
+// #include <iostream>
+
+// int main()
+// {
+//     int age = 22;
+
+//     std::cout << age << std::endl;
+
+//     age = 23;
+
+//     std::cout << age;
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include <iostream>
+
+// int main()
+// {
+//     int age = 22;
+//     int semester = 1;
+//     int marks = 85;
+
+//     std::cout << age << std::endl;
+//     std::cout << semester << std::endl;
+//     std::cout << marks << std::endl;
+
+//     return 0;
+// }
+
+
+
+
+#include <iostream>
+
+int main()
+{
+    int age = 22;
+    int semester = 1;
+    int marks = 85;
+
+    std::cout << "Age: " << age << std::endl;
+    std::cout << "Semester: " << semester << std::endl;
+    std::cout << "Marks: " << marks << std::endl;
+
+    return 0;
+}
