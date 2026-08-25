3ceefb05e910eb5d0ede4948dbadc412d7de962b
PRITAMVERMA <pritamkumar08102004@gmail.com>
2026-08-25 12:09:39 +0530

    day 02 learn function parameters

---
+++ day02/02_parameters.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+
+void sayHello(std::string name)
+{
+    std::cout << "Hello " << name << std::endl;
+}
+
+int main()
+{
+    sayHello("Rahul");
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include <iostream>
+
+// void printAge(int age)
+// {
+//     std::cout << "Age: " << age << std::endl;
+// }
+
+// int main()
+// {
+//     printAge(22);
+//     printAge(25);
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+// #include <iostream>
+
+// void printStudent(std::string name, int age)
+// {
+//     std::cout << "Name: " << name << std::endl;
+//     std::cout << "Age: " << age << std::endl;
+// }
+
+// int main()
+// {
+//     printStudent("Rahul", 22);
+
+//     return 0;
+// }
