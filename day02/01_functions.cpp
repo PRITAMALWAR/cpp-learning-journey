d00713c5504ee67e10877ca62ae5d6fd4a49bf39
PRITAMVERMA <pritamkumar08102004@gmail.com>
2026-08-25 12:06:35 +0530

    day 02 learn functions

---
+++ day02/01_functions.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+void sayHello()
+{
+    std::cout << "Hello C++!" << std::endl;
+}
+
+int main()
+{
+    sayHello();
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include <iostream>
+
+// void sayHello()
+// {
+//     std::cout << "Hello C++!" << std::endl;
+// }
+
+// int main()
+// {
+//     sayHello();
+//     sayHello();
+//     sayHello();
+
+//     return 0;
+// }
