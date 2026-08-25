07c00aebb05aeb324463b96870f8e83cb15f1b5e
PRITAMVERMA <pritamkumar08102004@gmail.com>
2026-08-25 12:15:27 +0530

    day 02 learn function return values

---
+++ day02/03_return.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int result = add(10, 20);
+
+    std::cout << "Result: " << result;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include <iostream>
+
+// int add(int a, int b)
+// {
+//     return a + b;
+// }
+
+// int main()
+// {
+//     std::cout << add(10, 20);
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include <iostream>
+
+// int square(int number)
+// {
+//     return number * number;
+// }
+
+// int main()
+// {
+//     int number;
+
+//     std::cout << "Enter number: ";
+//     std::cin >> number;
+
+//     int result = square(number);
+
+//     std::cout << "Square: " << result;
+
+//     return 0;
+// }
