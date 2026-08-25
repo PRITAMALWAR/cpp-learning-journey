2439f4f77dbb095d7ff5c2e156b47a4c7a8004f0
PRITAMVERMA <pritamkumar08102004@gmail.com>
2026-08-25 12:40:18 +0530

    day 02 learn arrays

---
+++ day02/06_arrays.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+int main()
+{
+    int numbers[5] = {10, 20, 30, 40, 50};
+
+    std::cout << numbers[0] << std::endl;
+    std::cout << numbers[1] << std::endl;
+    std::cout << numbers[2] << std::endl;
+    std::cout << numbers[3] << std::endl;
+    std::cout << numbers[4] << std::endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include <iostream>
+
+// int main()
+// {
+//     int numbers[5] = {10, 20, 30, 40, 50};
+
+//     std::cout << numbers[0] << std::endl;
+//     std::cout << numbers[1] << std::endl;
+//     std::cout << numbers[2] << std::endl;
+//     std::cout << numbers[3] << std::endl;
+//     std::cout << numbers[4] << std::endl;
+
+//     return 0;
+// }
+
+
+
+
+
+
