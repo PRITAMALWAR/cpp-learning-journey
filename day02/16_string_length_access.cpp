89583770825d331af44817ddd44200ba18c06d2c
PRITAMVERMA <pritamkumar08102004@gmail.com>
2026-08-25 22:50:22 +0530

    string length and character access

---
+++ day02/16_string_length_access.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string word = "Programming";
+
+    std::cout << word.length();
+
+    return 0;
+}
+
+
+
+
+
+// Print first and last character
+
+// #include <iostream>
+// #include <string>
+
+// int main()
+// {
+//     std::string word = "Programming";
+
+//     std::cout << "First: " << word[0] << std::endl;
+
+//     std::cout << "Last: "
+//               << word[word.length() - 1];
+
+//     return 0;
+// }
