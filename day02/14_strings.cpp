07fc7cc35076ec3e6b2985f5dbe9549cc5a244cc
PRITAMVERMA <pritamkumar08102004@gmail.com>
2026-08-25 22:39:29 +0530

    learn strings basics

---
+++ day02/14_strings.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string name = "Rahul";
+
+    std::cout << name;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+// string input
+
+// #include <iostream>
+// #include <string>
+
+// int main()
+// {
+//     std::string name;
+
+//     std::cout << "Enter your name: ";
+//     std::cin >> name;
+
+//     std::cout << "Hello " << name;
+
+//     return 0;
+// }
+
+
+
+
+// Traverse a string
+
+// #include <iostream>
+// #include <string>
+
+// int main()
+// {
+//     std::string name = "Rahul";
+
+//     for (int i = 0; i < name.length(); i++)
+//     {
+//         std::cout << name[i] << std::endl;
+//     }
+
+//     return 0;
+// }
