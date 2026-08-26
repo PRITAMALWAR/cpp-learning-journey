7893bfe13bd97e730264429a293b415208e2c5a8
PRITAMVERMA <pritamkumar08102004@gmail.com>
2026-08-26 17:45:07 +0530

    learn pointers

---
+++ day03/02_pointer.cpp
@@ -0,0 +1,45 @@
+// Create your first pointer
+
+#include <iostream>
+
+int main()
+{
+    int age = 22;
+
+    int* ptr = &age;
+
+    std::cout << "Age: " << age << std::endl;
+    std::cout << "Address: " << &age << std::endl;
+    std::cout << "Pointer: " << ptr << std::endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+// Pointer and original variable
+
+// #include <iostream>
+
+// int main()
+// {
+//     int number = 10;
+
+//     int* ptr = &number;
+
+//     std::cout << "Before: " << number << std::endl;
+
+//     *ptr = 50;
+
+//     std::cout << "After: " << number;
+
+//     return 0;
+// }
